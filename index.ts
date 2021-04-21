55e3cbf0217604c5beefe713b13fef934cf451b4
kuo7288 <kuo7288@gmail.com>
2021-04-21 10:45:38 -0400

    3 files modified, 2 added

--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import "./style.css";
 // import * from "url";
 import { PathDetail } from "./interfaces";
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./example";
+
 const myUrl = new URL("sandra://x/y/z.py?r=preRR&test=3");
 const query = myUrl.searchParams;
 const parama = new URLSearchParams(myUrl.search);
@@ -80,71 +84,4 @@ console.log("test");
 console.log(`AASSDASD ${testList}`);
 console.log(`AASSDASD ${mySet.values().next()}`);
 
-  
-import React from 'react';
-import DOM from 'react-dom'
-import Autocomplete from 'react-autocomplete'
-import { getStates, fakeRequest } from 'react-autocomplete/lib/utils'
-
-class App extends React.Component {
-
-  state = {
-    value: '',
-    unitedStates: getStates(),
-  }
-
-  requestTimer = null
-
-  render() {
-    return (
-      <div>
-        <h1>Async Data</h1>
-        <p>
-          Autocomplete works great with async data by allowing you to pass in
-          items. The <code>onChange</code> event provides you the value to make
-          a server request with, then change state and pass in new items, it will
-          attempt to autocomplete the first one.
-        </p>
-        <label htmlFor="states-autocomplete">Choose a state from the US</label>
-        <Autocomplete
-          inputProps={{ id: 'states-autocomplete' }}
-          wrapperStyle={{ position: 'relative', display: 'inline-block' }}
-          value={this.state.value}
-          items={this.state.unitedStates}
-          getItemValue={(item) => item.name}
-          onSelect={(value, item) => {
-            // set the menu to only the selected item
-            this.setState({ value, unitedStates: [ item ] })
-            // or you could reset it to a default list again
-            // this.setState({ unitedStates: getStates() })
-          }}
-          onChange={(event, value) => {
-            this.setState({ value })
-            clearTimeout(this.requestTimer)
-            this.requestTimer = fakeRequest(value, (items) => {
-              this.setState({ unitedStates: items })
-            })
-          }}
-          renderMenu={children => (
-            <div className="menu">
-              {children}
-            </div>
-          )}
-          renderItem={(item, isHighlighted) => (
-            <div
-              className={`item ${isHighlighted ? 'item-highlighted' : ''}`}
-              key={item.abbr}
-            >{item.name}</div>
-          )}
-        />
-      </div>
-    )
-  }
-}
-
-DOM.render(<App/>, document.getElementById('container'))
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById("app");
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-
+//ReactDOM.render(<App />, document.getElementById("app"));
